dd195aa4f0bf4b928d72ce664ceefcb283c6cd0f
Wirachai <wirachai.me@gmail.com>
2017-01-19 00:16:01 +0700

    Add Delegate and Lambda example

---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,37 @@
+﻿using LambdaExpression.SampleDelegates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicDelegate basic = new BasicDelegate();
+            basic.TestDelegate();
+            basic.TestAnonymousFunction();
+
+            Button button = new Button();
+            button.Text = "Click me";
+            button.Click += new EventHandler(OnClicked);
+            button.Click += new EventHandler((object sender, EventArgs e) => { Console.WriteLine("Test"); });
+
+            Form form = new Form();
+            form.AutoSize = true;
+            form.Controls.Add(button);
+            Application.Run(form);
+
+            Console.Read();
+        }
+
+        static void OnClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Clicked");
+        }
+    }
+}
---
+++ LambdaExpression/LambdaExpression/SampleDelegates/BasicDelegate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression.SampleDelegates
+{
+    public class BasicDelegate
+    {
+        private delegate int CalculationDelegate(int x, int y);
+        private int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        private int Divide(int dividend, int divisor)
+        {
+            return dividend / divisor;
+        }
+
+        public void TestDelegate()
+        {
+            CalculationDelegate calculation = new CalculationDelegate(Multiply);
+            Console.WriteLine(calculation(100, 2));
+            calculation = new CalculationDelegate(Divide);
+            Console.WriteLine(calculation(100, 2));
+        }
+
+        public void TestAnonymousFunction()
+        {
+            CalculationDelegate calculation;
+
+            calculation = new CalculationDelegate(delegate (int x, int y) { return Math.Max(x, y); });
+            Console.WriteLine(calculation(100, 200));
+
+            Func<int, int, int> func = delegate (int x, int y) { return Math.Min(x, y); };
+            calculation = new CalculationDelegate(func);
+            Console.WriteLine(calculation(100, 200));
+        }
+    }
+}
