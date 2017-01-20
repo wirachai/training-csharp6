b3d3147754b6739a89af5b1f16d2a01269ecf03c
Wirachai <wirachai.me@gmail.com>
2017-01-20 23:11:27 +0700

    Add C# 6 Workshop

---
+++ CSharp6/CSharp6/Logger/ConsoleLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp6.Logger
+{
+    public class ConsoleLogger
+    {
+        public string GetLogMessage(DateTime timestamp, LogLevel level, string message, string stackTrace)
+        {
+            var stackTraceMessage = stackTrace != null ? stackTrace : null;
+            if (stackTraceMessage == null)
+            {
+                return string.Format("{0:yyyy-MM-dd HH:mm:ss.ffff} - {1}: {2}", timestamp, level, message);
+            }
+            return string.Format("{0:yyyy-MM-dd HH:mm:ss.ffff} - {1}: {2}\n{3}", timestamp, level, message, stackTraceMessage);
+        }
+
+        public void Write(LogLevel level, string message)
+        {
+            DateTime timestamp = DateTime.Now;
+            var logMessage = GetLogMessage(timestamp, level, message, null);
+            Console.WriteLine(logMessage);
+        }
+
+        public void Write(LogLevel level, Exception ex)
+        {
+            string message = ex != null ? ex.Message : string.Empty;
+            string stackTrace = ex != null ? ex.StackTrace : null;
+
+            DateTime timestamp = DateTime.Now;
+            var logMessage = GetLogMessage(timestamp, level, message, stackTrace);
+            Console.WriteLine(logMessage);
+        }
+    }
+}
---
+++ CSharp6/CSharp6/Program.cs
@@ -0,0 +1,29 @@
+﻿using CSharp6.Logger;
+using System;
+
+namespace CSharp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleLogger logger = new ConsoleLogger();
+
+            try
+            {
+                logger.Write(LogLevel.Trace, "Start at " + DateTime.Now);
+
+                int y = 0;
+                int x = 10 / y;
+
+                logger.Write(LogLevel.Trace, "Finish at " + DateTime.Now);
+            }
+            catch (Exception ex)
+            {
+                logger.Write(LogLevel.Error, ex);
+            }
+
+            Console.Read();
+        }
+    }
+}
